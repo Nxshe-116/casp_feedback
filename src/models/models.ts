67243363de7f14d55b1717f9112dd6a218843680
Nashe Chagumaira <77992898+Nxshe-116@users.noreply.github.com>
2025-04-01 16:56:56 +1100

    fixed timestamp

--- src/models/models.ts
+++ src/models/models.ts
@@ -2,18 +2,15 @@ export interface Recommendation {
   id: string;
   name: string;
   title: string;
-
   text: string;
+  createdAt: string; 
 }
-
-
-
 
 export interface Notifications {
   notifId: string;
   userId: string;
   title: string;
   message: string;
-  timestamp: Date;
+  timestamp: string;  
   isRead: boolean;
 }
