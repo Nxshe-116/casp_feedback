70f8d658a2acbf7189d87c8a109cb0eaea11b4a8
Nashe Chagumaira <77992898+Nxshe-116@users.noreply.github.com>
2025-04-01 14:47:19 +1100

    Fixed atuff

---
+++ .angular/cache/19.2.5/C.A.S.P_web/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,39 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell
+} from "./chunk-CLPN2HY3.js";
+import "./chunk-AZDLM3QE.js";
+import "./chunk-PHYEB2DC.js";
+import "./chunk-HSKVOKEL.js";
+import "./chunk-R4SWTWZY.js";
+import "./chunk-4RDLTH23.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  withAppShell,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
